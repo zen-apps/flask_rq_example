f24b7024870c6e98aabe15e16d4f7d9b656831ec
Josh J <joshjanzen@gmail.com>
2021-02-17 19:39:09 -0600

    Create worker.py

---
+++ flask-app/worker.py
@@ -0,0 +1,15 @@
+import redis
+from rq import Worker, Queue, Connection
+
+listen = ['default']
+
+try:
+    conn = redis.from_url('redis://redis:6379/0')
+    print('REDIS', conn.config_get('maxmemory'))
+except Exception as e:
+    print('worker', e)
+
+if __name__ == '__main__':
+    with Connection(conn):
+        worker = Worker(map(Queue, listen))
+        worker.work()
