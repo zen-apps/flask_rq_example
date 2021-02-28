a5adf6c6b2f849e464137e8677b8f8923cac8a9e
same7ammar <mr.same7.ammar@gmail.com>
2021-02-28 12:01:26 +0300

    Update worker.py
    fix to make redis to connect to locally. from container .

--- flask-app/worker.py
+++ flask-app/worker.py
@@ -4,7 +4,7 @@ from rq import Worker, Queue, Connection
 listen = ['default']
 
 try:
-    conn = redis.from_url('redis://redis:6379/0')
+    conn = redis.from_url('redis://localhost:6379/0')
     print('REDIS', conn.config_get('maxmemory'))
 except Exception as e:
     print('worker', e)
